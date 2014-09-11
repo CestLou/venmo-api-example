d2dc823e6936ee6ab4938dc9f643099c7c0a307a
Andrew Khor <andrew.khow@venmo.com>
2014-09-11 18:16:43 -0400

    Milestone 2: Can make payments

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,5 +3,8 @@ class MainController < ApplicationController
     @user = Venmo.authenticate params[:code]
     @info = @user.get_info
     @friends = @user.get_friends
+    @other_user = @user.get_user 145434160922624933
+    @payment = @user.make_payment ({ :note => 'A message to accompany the payment.', :amount => '0.10', :user_id => 145434160922624933 })
+    puts @payment.inspect
   end
 end
