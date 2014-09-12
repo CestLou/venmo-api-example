26dc68419ebb4afb2731355a5919de1606bdf4d2
Andrew Khor <andrew.khow@venmo.com>
2014-09-12 16:18:06 -0400

    v0.0.3

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,10 +1,19 @@
 class MainController < ApplicationController
   def index
     @user = Venmo.authenticate params[:code]
-    @info = @user.get_info
-    @friends = @user.get_friends
-    @other_user = @user.get_user 145434160922624933
-    @payment = @user.make_payment ({ :note => 'A message to accompany the payment.', :amount => '0.10', :user_id => 145434160922624933 })
-    puts @payment.inspect
+    if @user
+      @info = @user.get_info
+      @friends = @user.get_friends
+      @other_user = @user.get_user 145434160922624933
+      @other_user_client = Venmo.get_user 145434160922624933, @user.access_token
+
+      @payment = @user.make_payment ({ :note => 'A message to accompany the payment.', :amount => '0.10', :user_id => 145434160922624933 })
+
+      @payment2 = @user.get_payment 1322585332520059420
+      @payment2_client = Venmo.get_payment 1322585332520059420, @user.access_token
+
+      @recent_payments = @user.get_recent_payments
+      @recent_payments_client = Venmo.get_recent_payments @user.access_token
+    end
   end
 end
